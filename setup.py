e2ddcc8b6e7bef2fe90313ced29552849f207ac8
SapuSeven <sapuseven@gmail.com>
2021-04-25 21:26:02 +0200

    Add shell script

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="keepass-dmenu",
     version="1.0.0",
     description="Provides a dmenu-compatible interface for KeePass databases.",
-    scripts=["keepass-dmenu.py"],
+    scripts=["keepass-dmenu.py", "keepass-dmenu"],
     install_requires=[
         "pykeepass_cache",
         "pyotp",
